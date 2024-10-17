e43726bb04b8205e8600f34053657ad982989457
GhanashyaMAhire <ghanshamahire.5558@gmail.com>
2024-10-17 10:27:18 +0530

    updated

---
+++ 17.october.2024/lopp2.py
@@ -0,0 +1,6 @@
+fruit = input("Enter the fruit your want : ")
+
+fruits = ['apple','mango','banana','dragon']
+for i in fruits:
+    if i == fruit:
+        print("Yes, Fruits is available for you")
