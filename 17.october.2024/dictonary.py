d2344c39b11f5989ae3b612e3b7a1bed286fbdcd
GhanashyaMAhire <ghanshamahire.5558@gmail.com>
2024-10-17 09:21:45 +0530

    dictonary method

--- 17.october.2024/dictonary.py
+++ 17.october.2024/dictonary.py
@@ -3,3 +3,8 @@ this_dictionry = {
     "model" : "Mustang",
     "year" : 1920
 }
+print(this_dictionry["brand"])
+print(this_dictionry["model"])
+print(this_dictionry["year"])
+print(this_dictionry.values())
+print(this_dictionry.keys())
